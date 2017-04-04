634cc0bc411b61910a2b9281bcfcc6fa18abffd3
Bahzad Khosravifar <Bahzad Khosravifar>
2017-04-04 13:11:46 +0430

    first init

---
+++ src/OwinSelfHostedWebSiteSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace OwinSelfHostedWebSiteSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var baseUrl = "http://localhost:4000";
+
+            // Start OWIN host
+            using (WebApp.Start<Startup>(new StartOptions(baseUrl)))
+            {
+                // Create HttpCient and make a request to api/values
+                HttpClient client = new HttpClient();
+                //client.BaseAddress = new Uri(baseUrl);
+                
+                var response = client.GetAsync(baseUrl+"/index.html").Result;
+                if (response != null)
+                {
+                    Console.WriteLine("Information from service: {0}", "stackoverflow sample page"); //response.Content.ReadAsStringAsync().Result);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Impossible to connect to service");
+                }
+                // Launch the browser
+                Console.WriteLine();
+                Process.Start(baseUrl);
+                Console.WriteLine("Press ENTER to stop the server and close app...");
+                Console.ReadLine(); // Keeps the host from disposing immediately
+            }
+        }
+    }
+}
---
+++ src/OwinSelfHostedWebSiteSample/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using OwinSelfHostedWebSiteSample;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace OwinSelfHostedWebSiteSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+
+            var physicalFileSystem = new PhysicalFileSystem(@".\www"); //. = root, Web = your physical directory that contains all other static content, see prev step
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" }; //put whatever default pages you like here
+            app.UseFileServer(options);
+        }
+
+
+    }
+}
